50cb26038eb63e338f061b270db9b0fcda8efd0f
MrGarlic1 <ben@samans.com>
2025-09-04 16:53:55 -0400

    fix: dir structure

--- autorun.sh
+++ autorun.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 SOURCE_DIR="$(dirname "$0")"
 SOURCE_DIR="$(readlink -e $SOURCE_DIR/../..)"
+echo $SOURCE_DIR
 cd "$SOURCE_DIR"
 
-if [ ! -f ./../.venv/bin/activate ]; then
+if [ ! -f .venv/bin/activate ]; then
     python3.13 -m venv ./.venv
     source ./.venv/bin/activate
     python3.13 -m pip install -r ./requirements.txt
